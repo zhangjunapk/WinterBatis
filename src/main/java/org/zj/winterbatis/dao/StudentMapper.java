7daf5a0ff3627382d5ae8e01d72ed572e447660f
张君 <1028191676@qq.com>
2018-07-08 20:28:14 +0800

    浏览器发送请求到服务器后，为controller动态注入request response 生成动态sql controller返回json的支持 浏览器发送请求到服务器后，为需要调用的方法注入参数列表(传统表单以及jsoon)-->传统表单测试了  json未测试

--- src/main/java/org/zj/winterbatis/dao/StudentMapper.java
+++ src/main/java/org/zj/winterbatis/dao/StudentMapper.java
@@ -1,7 +1,6 @@
 package org.zj.winterbatis.dao;
 
-import org.zj.winterbatis.annotation.Mapper;
-import org.zj.winterbatis.annotation.Select;
+import org.zj.winterbatis.annotation.*;
 import org.zj.winterbatis.bean.Student;
 
 import java.util.List;
@@ -14,4 +13,17 @@ public interface StudentMapper {
 
     @Select("select * from student")
     List<Student> findAll();
+
+    @Select("select * from student where username=${username}")
+    List<Student> findCondition(Student s);
+
+
+    @Insert("insert into student(username,password) values(${username},${password})")
+    boolean insert(Student student);
+
+    @Delete("delete from student where username=${username}")
+    boolean delete(Student s);
+
+    @Update("update student set username=${username}")
+    boolean update(Student s);
 }
